24914bdd9c8a3b59c10bbe3ab4b309817e4388cc
mitra <87939351+mitras-git@users.noreply.github.com>
2023-01-04 02:50:58 +0300

    Add comments to outro.py

--- outro.py
+++ outro.py
@@ -3,9 +3,11 @@ from playsound import playsound
 from multiprocessing import Process
 import time
 
+#Function that plays the required outro music sound file
 def song():
     playsound("outro.mp3")
 
+#While loop to initiate the shut down countdown
 def counterLoop():
     counter = 15
     while counter >= 0:
@@ -14,9 +16,9 @@ def counterLoop():
         counter -= 1
 
 if __name__ == '__main__':
-    p1 = Process(target=song)
+    p1 = Process(target=song) #Defining the first thread to target the song() function
     p1.start()
-    p2 = Process(target=counterLoop)
+    p2 = Process(target=counterLoop) #Defining the second thread to target the counterLoop() function
     p2.start()
     p2.join()
     os.system("pmset sleepnow")
